ff5ceb5c53b248a2f192274286f23fdf7b80feb0
Mikk Mangus <mikk.mangus@pipedrive.com>
2020-12-03 07:26:55 +0200

    Day3 complete

--- day03/day03.js
+++ day03/day03.js
@@ -1,15 +1,46 @@
 const { readFileToString } = require('../lib/lib.js');
 
-function func1(numbers) {
-    return numbers;
+function traverse(map, xAmount = 3, yAmount = 1) {
+    let x = 0;
+    let y = 0;
+
+    let countTrees = 0;
+
+    for (let internalX = 0; true;) {
+        if (map[y] === undefined) {
+            return countTrees;
+        }
+        const char = map[y][internalX];
+
+        if (char === '#') {
+            countTrees++;
+        }
+
+        internalX += xAmount;
+        if (undefined === map[y][internalX]) {
+            internalX = internalX % map[y].length;
+        }
+
+        x += xAmount;
+        y += yAmount;
+    }
+
+    throw new Error(`Should not reach this ${x} ${y}`);
 }
 
 if (require.main === module) {
     (async () => {
         const input = await readFileToString('day03.txt');
-        const numbers = func1(input.split('\n').map(n => parseInt(n, 10)));
+        const map = input.split('\n');
 
-        console.log(numbers);
+        const results = [];
+        results.push(traverse(map, 1, 1));
+        results.push(traverse(map, 3, 1));
+        results.push(traverse(map, 5, 1));
+        results.push(traverse(map, 7, 1));
+        results.push(traverse(map, 1, 2));
+
+        console.log(results, results.reduce((acc, x) => acc * x, 1));
     })();
 }
 
--- day05/day05.js
+++ day05/day05.js
@@ -6,7 +6,7 @@ function func1(numbers) {
 
 if (require.main === module) {
     (async () => {
-        const input = await readFileToString('day04.txt');
+        const input = await readFileToString('input.txt');
         const numbers = func1(input.split('\n'));
 
         console.log(numbers);
