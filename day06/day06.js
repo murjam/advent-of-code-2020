6a1d7be17cfa85ef38a1bc4ba2d5a1d423542bd1
Mikk Mangus <mikk.mangus@pipedrive.com>
2020-12-06 10:25:28 +0200

    Day6 done, was a good sleep :)

--- day06/day06.js
+++ day06/day06.js
@@ -1,15 +1,36 @@
 const { readFileToString } = require('../lib/lib.js');
 
-function func1(numbers) {
-    return numbers;
+function questionCount(group) {
+    const people = group.split('\n');
+    const allYes = {};
+
+    people[0].split('').forEach(char => {
+        allYes[char] = true;
+    });
+
+    for (let i = 1; i < people.length; i++) {
+        const person = people[i];
+        for (const char in allYes) {
+            if (person.indexOf(char) === -1) {
+                delete allYes[char];
+            }
+        }
+    }
+
+    return Object.keys(allYes).length;
+}
+
+function countYes(groups) {
+    return groups.map(group => questionCount(group))
+        .reduce((sum, current) => sum + current, 0);
 }
 
 if (require.main === module) {
     (async () => {
         const input = await readFileToString('input.txt');
-        const numbers = func1(input.split('\n'));
+        const sum = countYes(input.split('\n\n'));
 
-        console.log(numbers);
+        console.log(sum);
     })();
 }
 
