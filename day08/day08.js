25310fce0b8565a9c246d7368147b55a9b7f99c5
Mikk Mangus <mikk.mangus@pipedrive.com>
2020-12-08 07:32:24 +0200

    Day8 complete

--- day08/day08.js
+++ day08/day08.js
@@ -1,15 +1,64 @@
 const { readFileToString } = require('../lib/lib.js');
 
-function func1(lines) {
-    return lines;
+let acc = 0;
+let iter = 0;
+let visitedIndices = [];
+
+function executeStep(command) {
+    const [operation, value] = command.split(' ');
+    switch (operation) {
+        case 'acc':
+            acc += parseInt(value);
+            return;
+        case 'jmp':
+            iter += parseInt(value) - 1;
+            return;
+        case 'nop':
+            return;
+        default:
+            throw new Error(`Unknown operation ${operation}`);
+    }
+}
+
+function doesTerminate(program) {
+    acc = 0;
+    iter = 0;
+    visitedIndices = [];
+    const last = program.length - 1;
+
+    for (; iter < program.length; iter++) {
+        const command = program[iter];
+        if (visitedIndices.includes(iter)) {
+            // loop detected
+            return false;
+        }
+        visitedIndices.push(iter);
+        executeStep(command);
+
+        if (iter === last) {
+            return true;
+        }
+    }
 }
 
 if (require.main === module) {
     (async () => {
         const input = await readFileToString('input.txt');
-        const numbers = func1(input.split('\n'));
+        const initialProgram = input.split('\n');
 
-        console.log(numbers);
+        for (let i = 0; i < initialProgram.length; i++) {
+            const [operation] = initialProgram[i].split(' ');
+
+            if (['nop', 'jmp'].includes(operation)) {
+                const clone = [...initialProgram];
+                clone[i] = clone[i].replace(operation, operation === 'nop' ? 'jmp' : 'nop');
+
+                if (doesTerminate(clone)) {
+                    console.log('terminated', acc);
+                    return;
+                }
+            }
+        }
     })();
 }
 
