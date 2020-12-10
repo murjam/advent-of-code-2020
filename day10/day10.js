e74a3898f2a6906f92acdfd9770a625c18e7c056
Mikk Mangus <mikk.mangus@pipedrive.com>
2020-12-10 14:40:20 +0200

    Lots of hassling and a different approach for day 10

--- day10/day10.js
+++ day10/day10.js
@@ -1,14 +1,12 @@
-const { readFileToString } = require('../lib/lib.js');
-
-
-function doSomething(line) {
-
-}
+const {readFileToString} = require('../lib/lib.js');
 
 function getDifferences(jolts) {
     const removable = [];
     const differences = {};
+    const groups = [];
     let last = 0;
+
+    let group = [];
 
     for (let i = 0; i < jolts.length; i++) {
         const num = jolts[i];
@@ -21,20 +19,25 @@ function getDifferences(jolts) {
             differences[key] = 1;
         }
 
-        if (diff === 1) {
+        if (diff === 1 && i !== jolts.length - 1 && i !== 0) {
             removable.push(num);
+        } else if (diff === 3) {
+            groups.push(group);
+            group = [];
         }
+        group.push(num);
 
         last = num;
     }
+    groups.push(group);
 
-    return { differences, removable };
+    return {differences, removable, groups};
 }
 
 function willPass(jolts) {
-    let last = 0;
+    let last = jolts[0];
 
-    for (let i = 0; i < jolts.length; i++) {
+    for (let i = 1; i < jolts.length; i++) {
         const num = jolts[i];
         const diff = num - last;
 
@@ -49,9 +52,13 @@ function willPass(jolts) {
 }
 
 function combinations(sorted, allRemovable) {
-    let count = 0;
-    const traverse = function(removed, removable) {
+    if (sorted.length < 3) {
+        return 1;
+    }
+    let count = 1;
+    const traverse = function (removed, removable) {
         const pass = willPass(sorted.filter(n => !removed.includes(n)));
+
         if (!removed.length && !removable.length) {
             return;
         }
@@ -73,21 +80,52 @@ function combinations(sorted, allRemovable) {
     return count;
 }
 
-function findAllPasses(sorted, removable) {
+const passes = {};
+
+function findAllPasses(sorted, removable, removed = []) {
     let count = 0;
 
-    for (let i = 0; i < removable.length ; i++) {
-         const removeThis = removable[i];
-         const sortedWithRemoved = sorted.filter((n) => n !== removeThis && !allRemoved.includes(n));
-         const pass = willPass(sortedWithRemoved);
+    for (let i = 0; i < removable.length; i++) {
+        const removeThis = removable[i];
+        const allRemoved = [...removed, removeThis];
+        const sortedWithRemoved = sorted.filter((n) => !allRemoved.includes(n));
+        const passId = `${sorted.join(',')}:${removed.join(',')}`;
 
-         if (pass) {
-             count++;
-             count += findAllPasses(sorted, removable.filter(n => !allRemoved.includes(n)), allRemoved);
-         }
-     }
+        if (passId in passes) {
+            continue;
+        }
+
+        const pass = willPass(sortedWithRemoved);
+        passes[passId] = pass;
+
+        if (pass) {
+            count++;
+            count += findAllPasses(sortedWithRemoved, removable.filter(n => !allRemoved.includes(n)), allRemoved);
+        }
+    }
 
     return count;
+}
+
+function generateCombinationsMap(sorted) {
+    const jolts = [0, ...sorted];
+
+    const combinationsMap = {};
+    for (let i = 0; i < jolts.length; i++) {
+        const number = jolts[i];
+
+        combinationsMap[number] = i === 0 ? 1 : 0;
+        if (`${number - 1}` in combinationsMap) {
+            combinationsMap[number] += combinationsMap[number - 1];
+        }
+        if (`${number - 2}` in combinationsMap) {
+            combinationsMap[number] += combinationsMap[number - 2];
+        }
+        if (`${number - 3}` in combinationsMap) {
+            combinationsMap[number] += combinationsMap[number - 3];
+        }
+    }
+    return combinationsMap;
 }
 
 
@@ -98,12 +136,25 @@ if (require.main === module) {
         let highest = sorted[sorted.length - 1];
         sorted.push(highest + 3);
 
-        const { differences, removable } = getDifferences(sorted);
+        const combinations = generateCombinationsMap(sorted);
+        const last = combinations[highest];
+        console.log(last);
 
-        console.log(combinations(sorted, removable));
+        // console.log(Object.values(map)[Object.keys(map).length - 1]);
+
+        // const { differences, removable, groups } = getDifferences(sorted);
         // const allPasses = findAllPasses(sorted, removable);
-
-        console.log({ differences, removable });
+        //
+        // const groupCounts = groups.map(group => {
+        //     const { removable: r2 } = getDifferences(group);
+        //     console.log('group', group, r2)
+        //     return combinations(group, r2);
+        // });
+        // const testGroup = groups[1];
+        // const { removable: groupRemovables } = getDifferences(testGroup);
+        // const testPasses = combinations(testGroup, groupRemovables);
+        //
+        // console.log({ groups, testGroup, testPasses, allPasses, groupCounts, result: groupCounts.reduce((acc, n) => acc * n, 1) });
     })();
 }
 
