a4accfafa6c51075e1605b177e167cb00e60f7fe
Mikk Mangus <mikk.mangus@pipedrive.com>
2020-12-18 08:45:59 +0200

    Day18 complete and brushed

--- day18/day18.js
+++ day18/day18.js
@@ -1,15 +1,51 @@
 const { readFileToString } = require('../lib/lib.js');
 
-function func1(lines) {
-    return lines;
+function calculateSimple(expression) {
+    let a;
+
+    eval(`a = ${expression};`);
+
+    return a;
+}
+
+function generateCalculate(regex, subCall) {
+    return (expression) => {
+        while (true) {
+            const matches = regex.exec(expression);
+
+            if (!matches) {
+                return subCall(expression);
+            }
+
+            const [all, capturedExpression] = matches;
+            const result = subCall(capturedExpression);
+            const before = expression.substr(0, matches.index);
+            const after = expression.substr(matches.index + all.length);
+
+            expression = `${before}${result}${after}`;
+        }
+    };
+}
+
+const calculatePlusFirst = generateCalculate(/(\d+ \+ \d+)/, calculateSimple);
+const calculateBracketsFirst = generateCalculate(/\(([^()]+)\)/, calculatePlusFirst);
+
+function sumAll(expressions) {
+    let sum = 0;
+
+    for (const expression of expressions) {
+        sum += calculateBracketsFirst(expression);
+    }
+
+    return sum;
 }
 
 if (require.main === module) {
     (async () => {
         const input = await readFileToString('input.txt');
-        const numbers = func1(input.split('\n'));
+        const result = sumAll(input.split('\n'));
 
-        console.log(numbers);
+        console.log(result);
     })();
 }
 
