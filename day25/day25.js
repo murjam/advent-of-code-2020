1cd4342752816e4366dcd8598717f30a11fec6a1
Mikk Mangus <mikk.mangus@pipedrive.com>
2020-12-25 13:18:21 +0200

    Day 25 complete

--- day25/day25.js
+++ day25/day25.js
@@ -1,15 +1,50 @@
 const { readFileToString } = require('../lib/lib.js');
 
-function func1(lines) {
-    return lines;
+function transform(subjectNumber, loopSize, initialValue = 1) {
+    const base = 20201227;
+    let value = initialValue;
+
+    for (let i = 0; i < loopSize; i++) {
+        value *= subjectNumber;
+        value %= base;
+    }
+
+    return value;
+}
+
+function findLoopSize(subjectNumber, publicKey) {
+    let lastValue = 1;
+    for (let loopSize = 1; loopSize < 1e9; loopSize++) {
+        const result = transform(subjectNumber, 1, lastValue);
+
+        if (publicKey === result) {
+            return loopSize;
+        }
+
+        lastValue = result;
+    }
+
+    return -1;
+}
+
+function reverseEngineer(lines) {
+    const [cardPublic, doorPublic] = lines.map(n => parseInt(n, 10));
+
+    const cardLoopSize = findLoopSize(7, cardPublic);
+    const doorLoopSize = findLoopSize(7, doorPublic);
+
+    const encryptionDoor = transform(cardPublic, doorLoopSize);
+    const encryptionCard = transform(doorPublic, cardLoopSize);
+
+    return { cardLoopSize, doorLoopSize, encryptionDoor, encryptionCard};
 }
 
 if (require.main === module) {
     (async () => {
         const input = await readFileToString('input.txt');
-        const numbers = func1(input.split('\n'));
+        const result = reverseEngineer(input.split('\n'));
 
-        console.log(numbers);
+        console.log(result);
     })();
 }
 
